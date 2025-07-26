04f9de7e425b5db359a2a581727a37bd8d5bd788
smorihira <mos.aif.0924@gmail.com>
2025-07-26 18:34:59 +0900

    ついき

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// 全てのリクエストに対して200ステータスコードを返すミドルウェア
+// JSON形式のリクエストボディをパースするミドルウェア
+app.use(express.json());
+
+// すべてのリクエストに対して200ステータスコードを返すミドルウェア
 app.use((req, res, next) => {
+  console.log(`Received ${req.method} request for ${req.url}`);
+  console.log('Headers:', req.headers);
+  console.log('Query:', req.query);
+  if (req.body) {
+    console.log('Body:', req.body);
+  }
   res.status(200).send('OK');
 });
 
